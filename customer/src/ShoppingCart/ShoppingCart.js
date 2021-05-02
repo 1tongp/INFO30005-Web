0d1fdf5ba8721fcf85ee083622b4a0c274b935d8
sally-zzz <jiazhang4@student.unimelb.edu.au>
2021-05-02 20:56:00 +1000

    responsive header and parts of shopping cart
    Header made responsive, but sub-dropdown of user icon need to be added. Shopping cart layout completed, prices replaced with replacement text.

--- customer/src/ShoppingCart/ShoppingCart.js
+++ customer/src/ShoppingCart/ShoppingCart.js
@@ -1,45 +1,89 @@
 import React from 'react';
 import '../styles.css';
+import logo from '../images/logo.png';
 // import 'antd/dist/antd.css';
-import { Layout, Button, Space} from 'antd';
+import { Layout, Button} from 'antd';
 import {
     ShoppingOutlined,
     CaretLeftOutlined,
-    CopyrightOutlined
+    CaretRightOutlined,
+    CopyrightOutlined,
+    UserOutlined,
+    MenuOutlined
 } from '@ant-design/icons';
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Content } = Layout;
 
 export default function shoppingCart () {
     return (
-        <Layout>
-            <Header>
+        <>
+            <Header className='header_container'>
+                
+                <img src={logo} className='logo'/>
+                <p className='header_loc'>Current Location: <a className='lc_url'> Union House</a></p>
+                    
+                <div className='mid_nav'>
+                    <input type="checkbox" id="n_check"></input>
+                    <div class="hamburger">
+                        <label for="n_check">
+                            <MenuOutlined className='icon'/>
+                        </label>
+                    </div>
+                    <div className='links'>
+                        <a className='header_text' href=''>HOME</a>
+                        <a className='header_text' href=''>MENU</a>
+                        <a className='icon' href=''><ShoppingOutlined /></a>
+                        <a className='icon' href=''><UserOutlined /></a>
+                    </div>
+                </div>
+                    
+                    {/* <div className='right_icons'> */}
+                        {/* <a className='icon' href=''><ShoppingOutlined /></a> */}
+                        {/* <a className='icon' href=''><UserOutlined /></a> */}
+                    {/* </div>  */}
 
             </Header>
             <Layout>
                 <Content className='container'>
                     <h1>MY BASKET</h1>
                     <div className='row'>
-                        <div className='item_list'>
-                            <div className='item'>
-                                sdfwe
-
+                        <div className='col_70'>
+                            <div className='item_list'>
+                                <div className='item'>
+                                    erifnoeri
+                                </div>
+                                <div className='item'>
+                                    erifnoeri
+                                </div>
                             </div>
+                            <Button className='grey_button'>
+                                <CaretLeftOutlined className='left_arrow'/> 
+                                CONTINUE ORDERING
+                            </Button>
                         </div>
 
-                        <div className='summary'>
-                            <h2>SUMMARY</h2>
-                            
-                            
-                            
+                        <br />
+                        <br />
 
+                        <div className='col_30'>
+                            <div className='summary'>
+                                <h2>SUMMARY <span className='small_grey_text'>qty</span></h2>
+                                <p>Item Name <span className='small_grey_text'>qty</span><span className='price'>$ XX.XX</span></p>
+                                <p>Item Name <span className='small_grey_text'>qty</span><span className='price'>$ XX.XX</span></p>
+                                <br />
+                                <p>Total <span className='price'>$ XX.XX</span></p>
+                                <p>Pick up at <a href=''><span>Union House</span></a></p>
+                                <br />
+                                <br />
+                                <Button type="primary">
+                                    PLACE ORDER
+                                    <CaretRightOutlined className='right_arrow'/> 
+                                </Button>
+                            </div>
                         </div>
 
                     </div>
                 </Content>
-                <Sider>
-                    
-                </Sider>
             </Layout>
 
             <Footer>
@@ -49,8 +93,7 @@ export default function shoppingCart () {
                 All Rights Reserved
                 </p>
             </Footer>
-
-        </Layout>
+        </>
     )
 }
 
--- customer/src/ShoppingCart/ShoppingCartEmpty.js
+++ customer/src/ShoppingCart/ShoppingCartEmpty.js
@@ -1,11 +1,14 @@
-import React from 'react';
 import '../styles.css';
+import logo from '../images/logo.png';
 // import 'antd/dist/antd.css';
-import { Layout, Button, Space} from 'antd';
+import { Layout, Button} from 'antd';
 import {
     ShoppingOutlined,
     CaretLeftOutlined,
-    CopyrightOutlined
+    CaretRightOutlined,
+    CopyrightOutlined,
+    UserOutlined,
+    MenuOutlined
 } from '@ant-design/icons';
 
 const { Header, Footer, Content } = Layout;
@@ -13,7 +16,30 @@ const { Header, Footer, Content } = Layout;
 export default function shoppingCartEmpty () {
     return (
         <Layout>
-            <Header>
+            <Header className='header_container'>
+                
+                <img src={logo} className='logo'/>
+                <p className='header_loc'>Current Location: <a className='lc_url'> Union House</a></p>
+                    
+                <div className='mid_nav'>
+                    <input type="checkbox" id="n_check"></input>
+                    <div class="hamburger">
+                        <label for="n_check">
+                            <MenuOutlined className='icon'/>
+                        </label>
+                    </div>
+                    <div className='links'>
+                        <a className='header_text' href=''>HOME</a>
+                        <a className='header_text' href=''>MENU</a>
+                        <a className='icon' href=''><ShoppingOutlined /></a>
+                        <a className='icon' href=''><UserOutlined /></a>
+                    </div>
+                </div>
+                    
+                    {/* <div className='right_icons'> */}
+                        {/* <a className='icon' href=''><ShoppingOutlined /></a> */}
+                        {/* <a className='icon' href=''><UserOutlined /></a> */}
+                    {/* </div>  */}
 
             </Header>
             
@@ -30,6 +56,7 @@ export default function shoppingCartEmpty () {
                     <p>
                         YOUR BASKET IS EMPTY : (
                     </p>
+
                     <br />
                     <br />
                     <br />
