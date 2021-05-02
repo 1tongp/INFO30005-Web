30ceefaf15a97d889fd3491e1edbe17174b0b510
xzzhan4 <xzzhan4@student.unimelb.edu.au>
2021-05-02 17:17:30 +1000

    push login page basic set with css

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import App from './cutomerPages/App.js';
 import customerMain from './cutomerPages/CustomerMain.js';
+import loginPage from './loginPage/Login.js';
 
 class Router extends React.Component{
     render(){
@@ -10,6 +11,7 @@ class Router extends React.Component{
                 <Switch>
                     <Route path="/" exact component={App}></Route>
                     <Route path="/customer" exact component={customerMain}></Route>
+                    <Route path="/login" exact component={loginPage}></Route>
 
                 </Switch>
             </BrowserRouter>
