2c627b3f405a96ffee67dd5cac8e6b992619ba2d
[Yitong <yitong0822@gmail.com>
2021-05-07 01:59:14 +1000

    add header link

--- customer/src/MenuPreview/anchor.js
+++ customer/src/MenuPreview/anchor.js
@@ -1,41 +1,18 @@
 import {useState, useEffect} from 'react'
 import {Button} from 'react-bootstrap';
-import {Divider, Drawer, PageHeader} from 'antd';
-// import 'antd/dist/antd.css';
 import axios from '../API/axios';
-
 import MenuPre from './Menu.js';
 import '../ShoppingCart/styles.css';
-
 import logo from '../images/logo.png';
-import { Layout, InputNumber, message,Card } from 'antd';
-import {Modal} from 'react-bootstrap';
-import {
-    ShoppingOutlined,
-    CopyrightOutlined,
-    UserOutlined,
-    LikeOutlined,
-    MenuOutlined
-} from '@ant-design/icons';
+import { Layout,Card } from 'antd';
+import {ShoppingOutlined, UserOutlined, MenuOutlined} from '@ant-design/icons';
 import '../loginPage/Login.css';
-const { Header, Footer, Content } = Layout;
-const {Meta} = Card;
+const { Header} = Layout;
 
-function onChange(value) {
-    console.log('changed', value);
-  }
 
 function MenuPreview(props){
     console.log(props);
-    const [drawerVisible, setDrawerVisible] = useState(false);
-    const [orders, setOrders] = useState([]);
     const [snacks, setSnacks] = useState([]);
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const [loginEmail, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
     useEffect(() => {
         //snackMenuGet
         axios.get('/snack').then(response => {
@@ -44,30 +21,11 @@ function MenuPreview(props){
         })
     }, [props.location.state.customer]);
 
-
     return (
-        // <>
-        //     <PageHeader title = {"Hi " + props.location.state.customer.givenName + ": Welcome to Snack in Van "}
-        //         extra = {[<Menu key='0' snacks={snacks} customer={props.location.state.customer.id} />,
-        //             <Button variant = "outline-primary" key="1"
-        //                 onClick={handleDrawerShow}>See Orders</Button>
-        //         ]}>
-        //     </PageHeader>
-        //     <Drawer visible = {drawerVisible}
-        //         closable = {true}
-        //         onClick = {handleDrawerClose}
-        //         width = {"60vw"}>
-        //         All Orders
-        //         <Divider />
-        //         <OrderList orders = {orders} />
-        //     </Drawer>
-        // </>
         <Layout>
-            <Header className='header_container'>
-                
+            <Header className='header_container'>  
                 <img src={logo} className='logo'/>
-                <p className='header_loc'>Current Location: <a className='lc_url'>{}</a></p>
-                    
+                <p className='header_loc'>Current Location: <a className='lc_url'>{}</a></p>                   
                 <div className='mid_nav'>
                     <input type='checkbox' id='n_check'></input>
                     <div class='hamburger'>
@@ -82,12 +40,7 @@ function MenuPreview(props){
                         <div className='drop'>
                             <a className='icon'><UserOutlined /></a>
                             <div className='u_drop_content'>
-                                {/* <a href=''>Log In</a>
-                                <a href=''>Sign Up</a> */}
-
-                                {/* after log in */}
-
-                                <Button href='/customer/login'>Log In</Button>
+                                <Button href='../'>Log In</Button>
                                 <Button href=''>Sign Up</Button>
 
                             </div>
@@ -97,8 +50,7 @@ function MenuPreview(props){
 
             </Header>
             <MenuPre key='0' snacks={snacks} />
-        </Layout>
-        
+        </Layout>  
     )
 }
 
