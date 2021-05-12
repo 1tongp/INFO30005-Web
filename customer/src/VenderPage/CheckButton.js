2bf01c689184db480e0e3e3f239bea76faceb2dc
sally-zzz <jiazhang4@student.unimelb.edu.au>
2021-05-12 22:37:17 +1000

    added modal to buttons
    Added modal to fulfilled and preparing buttons

--- customer/src/VenderPage/CheckButton.js
+++ customer/src/VenderPage/CheckButton.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import './component.css'
 import {CheckOutlined} from '@ant-design/icons';
+import { Modal } from 'antd';
+import 'antd/dist/antd.css';
 
 // It is the light green finish button for preparing order lists.
 
 class CheckButton extends React.Component{
-    render(){
-        return <button className="check"><CheckOutlined /></button>
-               
-                
+    state = {
+        modal1Visible: false,
+    };
 
+    setModal1Visible(modal1Visible) {
+        this.setState({ modal1Visible });
+    }
+
+    render(){
+        return (
+            <>
+            <button className="check" onClick={() => this.setModal1Visible(true)}>
+            <CheckOutlined />
+            </button>
+            <Modal className='popup'
+            centered
+            visible={this.state.modal1Visible}
+            onOk={() => this.setModal1Visible(false)}
+            onCancel={() => this.setModal1Visible(false)}
+            >
+            <p>Order Prepared?</p>
+
+          </Modal>
+          </>
+        )
             
     }
 }
