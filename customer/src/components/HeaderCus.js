dc4c7fa991bc9a55245bb035fb37679405b7891d
crystal0161 <jintongl1@student.unimelb.edu.au>
2021-05-15 18:23:29 +1000

    change the current location on main and menu page
    'please select a vendor' or the current location

--- customer/src/components/HeaderCus.js
+++ customer/src/components/HeaderCus.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 import {Button} from 'react-bootstrap';
 import axios from '../API/axios';
-import Menu from './Menu.js';
+import Menu from '../Menu/Menu.js';
 import '../ShoppingCart/styles.css';
 import logo from '../images/logo.png';
 import { Layout, message } from 'antd';
@@ -9,16 +9,20 @@ import {ShoppingOutlined, UserOutlined, MenuOutlined} from '@ant-design/icons';
 import '../loginPage/Login.css';
 const { Header } = Layout;
 
-export default function Header(props) {
+
+
+export default function HeaderCus(props) {
+
+    const [vendorAddress, setVendorAddress] = useState('');
 
     const onOrder = () => {
         //orderListGet
-        axios.get('/order?customer=' + props.location.state.customer.id).then(response =>{
-        console.log(props);
+        axios.get('/order?customer=' + props.data.location.state.customer.id).then(response =>{
+        console.log(props.data.location.state.customer.id);
         console.log(response);
         if(response.data){
             // props push the useful data
-            props.history.push('/customer/order', {customerOrders: response.data.customerOrders});
+            props.data.history.push('/customer/order', {customerOrders: response.data.customerOrders});
         }
         else{
           message.error(response.data.error)
@@ -27,12 +31,23 @@ export default function Header(props) {
         console.log(error)
       })
     }
-  
+
+    useEffect(() => {
+        if (props.data.location.state.vendor){
+            setVendorAddress(props.data.location.state.vendor.currentAddress)
+        }else {
+            setVendorAddress("Please select your vendor first!")
+        }
+        
+    },[props.data.location.state.vendor]);
+        
+    
     return (
         <div>
             <Header className='header_container'>
                 <img src={logo} className='logo'/>
-                <p className='header_loc'>Current Location: <a className='lc_url'>{}</a></p>                   
+
+                <p className='header_loc'>Current Location: <a className='lc_url'>{vendorAddress}</a></p>            
                 <div className='mid_nav'>
                     <input type='checkbox' id='n_check'></input>
                     <div class='hamburger'>
@@ -47,7 +62,7 @@ export default function Header(props) {
                         <div className='drop'>
                             <a className='icon'><UserOutlined /></a>
                             <div className='u_drop_content'>
-                                <Button href=''>Hi {props.location.state.customer.givenName}</Button>
+                                <Button href=''>Hi {props.data.location.state.customer.givenName}</Button>
                                 <Button href='' key="1" onClick = {onOrder}>My Order</Button>
                                 <Button href=''>Log Out</Button>
                             </div>
