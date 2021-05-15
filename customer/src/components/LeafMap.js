896d9c583d0f575a7f7ac4a80cbdb63c6c49b5e6
crystal0161 <jintongl1@student.unimelb.edu.au>
2021-05-15 15:26:04 +1000

    add person marker on map

--- customer/src/components/LeafMap.js
+++ customer/src/components/LeafMap.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Icon } from "leaflet" ;
 import logo from '../images/coffee-marker.png';
+import personlogo from '../images/maps-marker.png';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 export default function LeafMap(props) {
     const vendorIcon = new Icon({
         iconUrl: logo,
+        iconSize: [55,55],
+    })
+    const personIcon = new Icon({
+        iconUrl: personlogo,
         iconSize: [55,55],
     })
 
@@ -17,7 +22,7 @@ export default function LeafMap(props) {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[-37.7963, 144.9614]} icon = { vendorIcon }>
+                <Marker position={[-37.7963, 144.9614]} icon = { personIcon }>
                     <Popup>
                         You are here.
                     </Popup>
