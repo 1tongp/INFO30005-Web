524c48d6717f00cd085caf4385d483ae9e7de3a0
[Yitong <yitong0822@gmail.com>
2021-05-15 15:29:03 +1000

    add the footer components for customer side

---
+++ customer/src/components/Footer.js
@@ -0,0 +1,16 @@
+import '../ShoppingCart/styles.css';
+import {CopyrightOutlined} from '@ant-design/icons'
+
+export default function Footer(){
+    return(
+        <>
+            <Footer>
+                    <p>
+                    <CopyrightOutlined /> SNACKS IN A VAN
+                    <br />
+                    All Rights Reserved
+                    </p>
+            </Footer>
+        </>
+    )
+}
