cf8fc1c0ddbfdfa2614a992730c5c95eeb35492b
crystal0161 <jintongl1@student.unimelb.edu.au>
2021-05-23 12:21:19 +1000

    pop up window cannot work yet
    the order/create need to be changed to order/update

--- customer/src/components/OrderDetail.js
+++ customer/src/components/OrderDetail.js
@@ -2,13 +2,14 @@
 import React, { Component } from 'react'
 import { Modal } from 'react-bootstrap';
 import '../myOrderPage/MyOrder.css';
-import { Layout, Button, notification } from 'antd';
+import { Layout, Button, notification, InputNumber, Card } from 'antd';
 import { StarFilled, StarOutlined } from '@ant-design/icons';
 import CountUp from './CountUp.js';
 import { Model } from 'mongoose';
-// import axios from '../API/axios.js';
+import axios from '../API/axios.js';
 // import {useHistory} from "react-router-dom";
 const { Content } = Layout;
+const { Meta } = Card;
 
 // // line 25 createTime -> updateTime
 
@@ -134,6 +135,8 @@ export default class OrderDetail extends Component {
         super(props);
         console.log(this.props);
         this.state = {
+            menu: [],
+            order: [],
             editModalVisible: false,
             modalBody: <></>,
             diff: ""
@@ -144,6 +147,51 @@ export default class OrderDetail extends Component {
     handleEditClose = () => this.setState([{ editModalVisible: false }]);
     handleEditShow = () => this.setState([{ editModalVisible: true }]);
 
+    onChange = (index, event) => {
+        let newArray = [...this.state.order];
+        newArray[index] = event;
+        this.setState({ order: newArray });
+    }
+
+    onOrderSubmit = () => {
+        var submitOrder = []
+        var sumPrice = 0;
+        for (var i = 0; i < this.state.order.length; i++) {
+            if (Number.isFinite(this.state.order[i])) {
+                submitOrder.push({
+                    "snackName": this.state.menu[i].snackName,
+                    "qty": this.state.order[i],
+                    "snackPrice": this.state.menu[i].snackPrice
+                })
+                sumPrice += this.state.menu[i].snackPrice * this.state.menu[i]
+            }
+        }
+
+        if (submitOrder.length === 0) {
+            this.setState({editModalVisible: false});
+            alert("Please do not submit empty order")
+        } else {
+            console.log(this.props);
+            axios.post('/order/create', {
+                customer: this.props.order.customer._id,
+                vendor: this.props.order.vendor._id, // will be changed in the future
+                snacksList: submitOrder,
+                totalPrice: sumPrice
+            }).then(response => {
+                console.log(response);
+                if (response.data.message === "created a new order") {
+                    // change the message print to a pop up page
+                    alert("Order has been places! You can check your order and view previous orders in My Order page")
+                    this.setState({editModalVisible: false});
+                }
+                else {
+                    // change the message print to a pop up page
+                    alert("Order placing errored!")
+                }
+            })
+        }
+    }
+
     tick() {
         let now = new Date().getTime()
         let upd = Date.parse(this.props.order.updateTime)
@@ -152,6 +200,9 @@ export default class OrderDetail extends Component {
 
     // update each second
     componentDidMount() {
+        axios.get('/snack').then(response => {
+            this.setState({ menu: response.data.snacks })
+        })
         this.timerID = setInterval(
             () => this.tick(), 1000
         );
@@ -185,18 +236,59 @@ export default class OrderDetail extends Component {
     }
 
     renderEditModelBody = () => {
-        return (
-            <>
-                <Modal.Header closeButton>
-                    <Modal.Title>{"Your order id:" + this.props.order._id}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>test</p>
-                </Modal.Body>
+        if (this.props.order.status === "outstanding") {
+            return (
+                <>  <div className='change-container'>
+                    <div className='change-popup'>
+                        <Modal.Header>
+                            <Modal.Title>Menu</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {this.state.menu.map((snack, index) => (
+                                <Card cover={<img alt="example" src={snack.snackPhotoPath} />} style={{ marginBottom: "1vh" }} size={"small"} key={snack._id}>
+                                    <Meta
+                                        title={snack.snackName + "      $" + snack.snackPrice}
+                                    />
+                                    <InputNumber key={snack._id} min={0} defaultValue={0} onChange={e => this.onChange(index, e)} />
+                                </Card>
+                            ))}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="primary" onClick={() => this.onOrderSubmit()}>
+                                Update
+                            </Button>
+                            <Button onClick={() => this.handleEditClose()}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </div>
+                </div>
+                </>
 
-            </>
+            )
 
-        )
+        } else {
+            return (
+                <>  <div className='change-container'>
+                    <div className='change-popup'>
+                        <Modal.Header>
+                            <Modal.Title>{"Your order id:" + this.props.order._id}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p>comment and rating</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={() => this.handleEditClose()}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </div>
+                </div>
+                </>
+
+            )
+
+        }
     }
 
 
