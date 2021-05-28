ee25df3f1e71494d3e8097777d313a771ce46166
[Yitong <yitong0822@gmail.com>
2021-05-28 23:07:20 +1000

    changed the popup remainder for email/password incorrect

--- customer/src/components/HeaderCus.js
+++ customer/src/components/HeaderCus.js
@@ -23,6 +23,7 @@ export default function HeaderCus(props) {
     const [buttonMyOrder, setButtonMyOrder] = useState([]);
     const [buttonLogOut, setButtonLogOut] = useState([]);
     const [buttonMyProfile, setButtonProfile] = useState([]);
+    const [buttonCart, setButtonCart] = useState([]);
 
     const toLogin = () =>{
         props.data.history.push('../');
@@ -77,6 +78,7 @@ export default function HeaderCus(props) {
             setButtonMyOrder([<Button href='' key="1" onClick = {onOrder}>My Order</Button>])
             setButtonProfile([<Button href='' key="1" onClick = {onProfile}> My Profile</Button>])
             setButtonLogOut([<Button href='' key="1" onClick = {toLogin}>Log Out</Button>])
+            setButtonCart([<Button href='' key="1" onClick = {onOrder}><ShoppingOutlined /></Button>])
         }else{
             setTitle([<Button href='' key="1" onClick = {toLogin}>Log In</Button>])
         }
@@ -156,7 +158,7 @@ export default function HeaderCus(props) {
                         <Button onClick={onCustomerLogin} id='btnMenu'> HOME </Button>
                         {/* {buttonHome} */}
                         <Button id='btnMenu'> MENU </Button>
-                        <a className='icon' href=''><ShoppingOutlined /></a>
+                        {buttonCart}
                         <div className='drop'>
                             <a className='icon'><UserOutlined /></a>
                             <div className='u_drop_content' >
