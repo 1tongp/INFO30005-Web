078d0943a51ca2cfdd0a8c024e971747980088ad
sally-zzz <jiazhang4@student.unimelb.edu.au>
2021-05-29 22:26:02 +1000

    order detail popup

--- customer/src/VenderPage/FinishedOrderDetail.js
+++ customer/src/VenderPage/FinishedOrderDetail.js
@@ -14,25 +14,29 @@ class FinishedOrderDetail extends React.Component {
             <li>Customer Id: {this.props.children.customer}</li>
             <li>Order Create Time: {this.props.children.createTime.slice(0, 10)} {this.props.children.createTime.slice(11, 19)}</li>
             <li>Order Fulfilled Time: {this.props.children.updateTime.slice(0, 10)} {this.props.children.updateTime.slice(11, 19)}</li>
-            <br />
-
+            
+            <p className="font--discount">{(this.props.children.discount) ? "Discount Applied" : "No Discount Applied"}</p>
             <div className="cluster ">
-                <p className="font--totalprice">Orders: </p>
+            
+                {/* <p className="font--totalprice">Order Items: </p> */}
+                <p>
                 {
                     this.props.children.snacksList.map((singleSnack) => (
                         <li>{singleSnack.snackName} x {singleSnack.qty} ${singleSnack.snackPrice}</li>
                     ))
                 }
-                <p className="font--discount">{(this.props.children.discount) ? "Discount Applied" : "No Discount Applied"}</p>
+                </p>
                 <p className="font--totalprice">Total Price: ${this.props.children.totalPrice}</p>
             </div>
-            <div>
-                <p className="font--totalprice">FeedBack:</p>
-                   Service: {(this.props.children.ratings) ? this.props.children.ratings : "No Ratings"} (1 is terrible, 5 is great!)
+            <div className='feedback'>
+                {/* <p className="font--totalprice">Feedback:</p> */}
+                <p>
+                   Service Rating: {(this.props.children.ratings) ? this.props.children.ratings : "No Ratings"} 
+                   <br />
+                   Comments: {(this.props.children.comments) ? this.props.children.comments : "No Comments"}
+                </p>
                </div>
-            <p>
-                Comments: {(this.props.children.comments) ? this.props.children.comments : "No Comments"}
-            </p>
+            
         </div>
     }
 }
