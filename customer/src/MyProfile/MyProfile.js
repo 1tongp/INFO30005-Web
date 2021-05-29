e98a9a350f7cb3dcdd2df5d74ca8c40a7f6e0bac
[Yitong <yitong0822@gmail.com>
2021-05-29 18:26:19 +1000

    test direction

--- customer/src/MyProfile/MyProfile.js
+++ customer/src/MyProfile/MyProfile.js
@@ -35,7 +35,7 @@ export default function MyProfile (props) {
                 if(response.data.changeDetails){
                 // push the customer information
                     alert("success! Please Login again using your new details");
-                    history.push({
+                    history.repalce({
                         pathname: '/',});
                 }
                 else{
