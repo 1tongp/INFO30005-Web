a489426d5c0344b0739c79b2f7357253c5d06393
[Yitong <yitong0822@gmail.com>
2021-05-30 13:42:17 +1000

    added the unhashed login check

--- routes/customer.js
+++ routes/customer.js
@@ -12,5 +12,9 @@ CustomerRouter.get('/:id', customerController.customerDetailGet);
 // POST request to change the customer details
 CustomerRouter.post('/changeDetails/:id', customerController.customerChangeDetailsPost);
 
+// POST request to login as customer
 CustomerRouter.post('/login', customerController.customerLoginPost);
+
+// POST request for front end to go back to the home page
+CustomerRouter.post('/loginhash', customerController.customerLoginUnhashPost);
 module.exports = CustomerRouter;
