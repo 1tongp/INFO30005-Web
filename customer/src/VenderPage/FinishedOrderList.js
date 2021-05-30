598fc9952ec01ac2cf436279cee97a29a520b011
[Yitong <yitong0822@gmail.com>
2021-05-30 19:46:56 +1000

    added the customer given Name

--- customer/src/VenderPage/FinishedOrderList.js
+++ customer/src/VenderPage/FinishedOrderList.js
@@ -20,7 +20,7 @@ class FinishedOrderList extends React.Component {
                             <div className="container--finishedorderlist"> 
                                 <div className="container--basicinfo">
                                     <p>Order Id: {singleOrder._id}</p>
-                                    <p>Customer Id: {singleOrder.customer}</p>
+                                    <p>Customer Given Name: {singleOrder.customerName}</p>
                                     <p>{singleOrder.createTime.slice(0, 10)}  || {singleOrder.createTime.slice(11, 19)}</p>
                                     <p>Order Status: {singleOrder.status}</p>
                                 </div>
